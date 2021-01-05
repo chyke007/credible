ffc1699142a5e36d629b4eb05043011f2fe707b7
Nwachukwu Chibuike <nwachukwuchibuike2012@gmail.com>
2021-01-05 19:17:14 +0100

    Error resolving 1.3

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -72,7 +72,7 @@ class LoginTest extends TestCase
             $response
                 ->assertStatus(200)
                 ;
-                var_dump($response->json()['data']);
+                var_dump($response->json()['data']['user']);
                 $response = $this->withHeaders([
                     'Accept' => 'application/json',
                 ])->json('POST', '/api/login', 
